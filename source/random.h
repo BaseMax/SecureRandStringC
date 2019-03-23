bfa0df4b73b4c3299fddabc3ca5a908f8f9abee6
Max Base <MaxBaseCode@Gmail.Com>
2019-03-23 16:24:55 +0430

    Update random.h

--- source/random.h
+++ source/random.h
@@ -1,2 +1,10 @@
 /**
+*
+* @Name : SecureRandStringC.C
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2019-03-23
+* @Released under : https://github.com/BaseMax/SecureRandStringC/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/SecureRandStringC
+*
 **/
